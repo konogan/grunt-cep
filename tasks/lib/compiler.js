4ef6b60f9d6b15eb0df33a54e62fc166e120a0be
Konogan <konogan@gmail.com>
2015-03-19 17:33:44 +0100

    OSx update

--- tasks/lib/compiler.js
+++ tasks/lib/compiler.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-module.exports = function (grunt)
+module.exports = function(grunt)
 {
     'use strict';
     var path = require('path'),
@@ -25,16 +25,16 @@ module.exports = function (grunt)
     /**
      * Compiles the specified build.
      */
-    var compile = function (callback, build)
+    var compile = function(callback, build)
     {
         var tasks = [];
         grunt.log.writeln().writeln(('Compiling bundle for Adobe ' + build.products.join(', ').cyan + ' - ' + build.families.join(', ').cyan + '...').bold);
-        var  exec = require('child_process').exec;
+        var exec = require('child_process').exec;
         tasks.push(
             /**
              * Do some cleanup.
              */
-            function (callback)
+            function(callback)
             {
                 // Validate input/output files
                 if (typeof build.staging !== 'string' || !build.staging.length)
@@ -54,16 +54,13 @@ module.exports = function (grunt)
             /**
              * Copy all files.
              */
-            function (callback)
+            function(callback)
             {
                 // Copy all the content of the source folder over to the output folder
                 var message = 'Copying ' + build.source.cyan + ' folder...';
                 grunt.verbose.writeln(message).or.write(message);
-                //cep.utils.copy({ cwd: build.source }, build.staging + '/', '**/*.*');
-                //
-                //
-                //
-                var cmd = 'cp -Ri ' + build.source +'/ '+build.staging + '/';
+
+                var cmd = 'cp -Ri ' + build.source + '/ ' + build.staging + '/';
                 exec(cmd, function(error, result, code) {
                     grunt.verbose.or.ok();
                     callback();
@@ -73,7 +70,7 @@ module.exports = function (grunt)
             /**
              * Generate .debug file.
              */
-            function (callback)
+            function(callback)
             {
                 if (build.profile === 'debug' || build.profile === 'launch')
                 {
@@ -105,20 +102,20 @@ module.exports = function (grunt)
             /**
              * Generate appropriate manifest file.
              */
-            function (callback)
+            function(callback)
             {
                 require('./xml')(grunt).manifest(callback, build);
             }
         );
 
         // Run child tasks
-        async.series(tasks, function (err, result) { callback(err, result); });
+        async.series(tasks, function(err, result) { callback(err, result); });
     };
 
     /**
      * Launches debug with specified settings.
      */
-    var launch = function (callback, build, host)
+    var launch = function(callback, build, host)
     {
         var tasks = [],
             launch_config = {
@@ -134,13 +131,13 @@ module.exports = function (grunt)
             /**
              * Detect application executable location.
              */
-            function (callback)
+            function(callback)
             {
                 var folder_apps, folder_servicemgr;
 
                 if (global.IS_WINDOWS)
                 {
-                    folder_apps = path.join(process.env["PROGRAMFILES"], '/Adobe');
+                    folder_apps = path.join(process.env['PROGRAMFILES'], '/Adobe');
 
                     if (launch_config.family === 'CC')
                         folder_servicemgr = path.join(process.env['APPDATA'], '/Adobe/CEPServiceManager4/extensions');
@@ -163,13 +160,13 @@ module.exports = function (grunt)
                 // Application folder
                 var folder_family = {
                     'CC': 'CC',
-                    'CC2014': 'CC 2014',
+                    'CC2014': 'CC 2014'
                 };
 
                 var folder_name = launch_config.host.hasOwnProperty('folder') ? launch_config.host.folder : '/Adobe ' + launch_config.host.name + ' ' + folder_family[launch_config.family];
                 launch_config.app_folder = path.join(folder_apps, folder_name);
 
-                if (launch_config.family === 'CC' && launch_config.host.x64 && global.IS_X64)
+                if (launch_config.family === 'CC' && launch_config.host.x64 && global.IS_X64 && global.IS_WINDOWS)
                     launch_config.app_folder += ' (64 Bit)';
 
                 if (!grunt.file.exists(launch_config.app_folder))
@@ -193,7 +190,7 @@ module.exports = function (grunt)
             /**
              * Kill the specified host application process if it is running.
              */
-            function (callback)
+             function (callback)
             {
                 var options = {};
 
@@ -201,34 +198,38 @@ module.exports = function (grunt)
                 {
                     options.cmd = 'Taskkill';
                     options.args = ['/IM', launch_config.host.bin.win];
+                     grunt.verbose.writeln((options.cmd + ' ' + options.args.join(' ')).magenta)
+                      .or.write('Killing host application process...');
+                      var spawned = grunt.util.spawn(options, function (error, result, code)
+                      {
+                          grunt.verbose.or.ok();
+                          // Let some time pass so that application can be closed correctly
+                          // TODO: find a better way of doing this
+                          setTimeout(function ()
+                          {
+                              callback();
+                          }, 1000);
+                      });
+                      spawned.stdout.on('data', function (data) { grunt.verbose.writeln(data); });
+                      spawned.stderr.on('data', function (data) { grunt.verbose.writeln(data); });
                 }
                 else
                 {
-                    options.cmd = 'kill';
-                    options.args = ['-9', '`ps -ef | grep "' + launch_config.host.bin.mac + '" | grep -v grep | awk \'{print $2}\'`'];
-                }
-
-                grunt.verbose.writeln((options.cmd + ' ' + options.args.join(' ')).magenta)
-                .or.write('Killing host application process...');
-                var spawned = grunt.util.spawn(options, function (error, result, code)
-                {
-                    grunt.verbose.or.ok();
-
-                    // Let some time pass so that application can be closed correctly
-                    // TODO: find a better way of doing this
-                    setTimeout(function ()
-                    {
+                    var appMac = launch_config.host.bin.mac;
+                    var processMac = appMac.slice(0, - 4); // - .app
+                    var cmd = 'pkill -9 "' + processMac + '"';
+                    grunt.verbose.writeln((cmd).magenta).or.write('Killing host application process...');
+                    exec(cmd, function(error, result, code) {
+                        grunt.verbose.or.ok();
                         callback();
-                    }, 1000);
-                });
-                spawned.stdout.on('data', function (data) { grunt.verbose.writeln(data); });
-                spawned.stderr.on('data', function (data) { grunt.verbose.writeln(data); });
+                    });
+                }
             },
 
             /**
              * Set "PlayerDebugMode" flag in plist file or Windows registry.
              */
-            function (callback)
+            function(callback)
             {
                 var PLISTS,
                     options,
@@ -238,7 +239,7 @@ module.exports = function (grunt)
                 {
                     PLISTS = {
                         'CC': path.join(process.env['HOME'], '/Library/Preferences/com.adobe.CSXS.4.plist'),
-                        'CC2014': path.join(process.env['HOME'], '/Library/Preferences/com.adobe.CSXS.5.plist'),
+                        'CC2014': path.join(process.env['HOME'], '/Library/Preferences/com.adobe.CSXS.5.plist')
                     };
 
                     if (!PLISTS.hasOwnProperty(host_family))
@@ -253,7 +254,7 @@ module.exports = function (grunt)
                 {
                     PLISTS = {
                         'CC': 'HKEY_CURRENT_USER\\Software\\Adobe\\CSXS.4\\',
-                        'CC2014': 'HKEY_CURRENT_USER\\Software\\Adobe\\CSXS.5\\',
+                        'CC2014': 'HKEY_CURRENT_USER\\Software\\Adobe\\CSXS.5\\'
                     };
 
                     if (!PLISTS.hasOwnProperty(host_family))
@@ -267,7 +268,7 @@ module.exports = function (grunt)
 
                 grunt.verbose.writeln((options.cmd + ' ' + options.args.join(' ')).magenta)
                 .or.write('Setting OS debug mode...');
-                var spawned = grunt.util.spawn(options, function (error, result, code)
+                var spawned = grunt.util.spawn(options, function(error, result, code)
                 {
                     if (code !== 0)
                     {
@@ -276,17 +277,36 @@ module.exports = function (grunt)
                     }
                     else
                         grunt.verbose.or.ok();
-
-                    callback(error, result);
+                    // note for macUSER
+                    // Special notes for Mac 10.9 and higher
+                    //
+                    //Staring with Mac 10.9, Apple introduced a caching mechanism for plist files.
+                    //Your modifications to plist files does not take effect until the cache gets updated
+                    //(on a periodic basis, you cannot know
+                    //exactly when the update will happen). To make sure your modifications take effect, there are two methods.
+                    //
+                    // - Kill cfprefsd process. It will restart automatically. Then the update takes effect.
+                    // - Restart your Mac, or log out the current user and re-log in.
+                     if (global.IS_MAC && true) {
+                        var cmd = 'pkill -9 cfprefsd';
+                        grunt.verbose.writeln((cmd).magenta).or.write('Killing cfprefsd process for Mac 10.9 and higher');
+                        exec(cmd, function(error, result, code) {
+                            grunt.verbose.or.ok();
+                            callback();
+                        });
+                     }
+                     else {
+                      callback(error, result);
+                    }
                 });
-                spawned.stdout.on('data', function (data) { grunt.verbose.writeln(data); });
-                spawned.stderr.on('data', function (data) { grunt.verbose.writeln(data); });
+                spawned.stdout.on('data', function(data) { grunt.verbose.writeln(data); });
+                spawned.stderr.on('data', function(data) { grunt.verbose.writeln(data); });
             },
 
             /**
              * Install extension by copying files to the 'extensions' folder.
              */
-            function (callback)
+            function(callback)
             {
                 var message = 'Installing extension at ' + launch_config.install_path.cyan + '...';
                 grunt.verbose.writeln(message).or.write(message);
@@ -297,7 +317,7 @@ module.exports = function (grunt)
 
                 // Copy files over
 
-                var cmd = 'cp -Ri "' + build.staging +'"  "'+launch_config.install_path +'"';
+                var cmd = 'cp -Ri "' + build.staging + '"  "' + launch_config.install_path + '"';
                 exec(cmd, function(error, result, code) {
                     grunt.verbose.or.ok();
                 callback();
@@ -309,7 +329,7 @@ module.exports = function (grunt)
             /**
              * Launch the specified host application.
              */
-            function (callback)
+            function(callback)
             {
                 var options = {};
 
@@ -317,14 +337,14 @@ module.exports = function (grunt)
                 {
                     options = {
                         cmd: 'open',
-                        args: ['-F', '-n', "'"+launch_config.app_bin+"'"],
+                        args: ['-F', '-n', "'" + launch_config.app_bin + "'"]
                     };
                 }
                 else
                 {
                     options = {
                         cmd: 'explorer.exe',
-                        args: ['"' + launch_config.app_bin + '"'],
+                        args: ['"' + launch_config.app_bin + '"']
                     };
                 }
 
@@ -332,7 +352,7 @@ module.exports = function (grunt)
                 .or.write('Starting host application...');
 
 
-                exec(options.cmd + ' ' + options.args.join(' '), function (error, result, code)
+                exec(options.cmd + ' ' + options.args.join(' '), function(error, result, code)
                 {
                     grunt.verbose.or.ok();
                     // REVIEW: command seems to fail on Windows even if in practice it works just fine...
@@ -343,7 +363,7 @@ module.exports = function (grunt)
         );
 
         // Run child tasks
-        async.series(tasks, function (err, result) { callback(err, result); });
+        async.series(tasks, function(err, result) { callback(err, result); });
     };
 
     // Public interface
